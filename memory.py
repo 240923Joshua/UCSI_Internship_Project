30f1c048b9430627b9935122e6d503a5e1d91612
Pikathebot <82572216+Pikathebot@users.noreply.github.com>
2025-12-30 08:23:54 +0530

    added question in memory

--- memory.py
+++ memory.py
@@ -3,5 +3,8 @@ LAST_MESSAGE = {}
 def get_last_message(user_id, internship_id):
     return LAST_MESSAGE.get((user_id, internship_id))
 
-def set_last_message(user_id, internship_id, message):
-    LAST_MESSAGE[(user_id, internship_id)] = message
+def set_last_message(user_id, internship_id, message, question):
+    LAST_MESSAGE[(user_id, internship_id)] = {
+        "message": message,
+        "question": question
+    }
